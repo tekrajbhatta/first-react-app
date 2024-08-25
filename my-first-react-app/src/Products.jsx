2db2fbd1f44be87b911a0c40bf93f5d9ffdc015f
Tek Raj Bhatt <tekbhatt1@gmail.com>
2024-08-25 18:05:43 +0545

    Counter increase on button click

---
+++ my-first-react-app/src/Products.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Products() {
+  return (
+    <div className='text-white'>
+      <div className='w-full h-96 bg-zinc-800'>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Products
